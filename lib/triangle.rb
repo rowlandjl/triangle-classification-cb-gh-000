f3ddb7ae1cec21d3b0172d04c410703c15983a4c
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-29 20:14:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,14 @@
 class Triangle
 
-  def initialize(side1:, side2:, side3:)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(l1, l2, l3)
+    @lengths
   end
 
   def kind
     if @side1 == @side2 && @side2 == @side3
       :equilateral
     end
-  end 
+  end
 
   class TriangleError < StandardError
 
