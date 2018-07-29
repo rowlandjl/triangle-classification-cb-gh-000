327013fa3baa1b9804563e5099405aa366a9fc92
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-29 20:17:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,13 @@ class Triangle
   def kind
     if lengths.any? {|l| l <= 0 } || (lengths[1] + lengths[2] <= lengths[0] || lengths[0] + lengths[2] <= lengths[1] || lengths[0] + lengths[1] <= lengths[2])
       raise TriangleError
-      
+    elsif lengths.all? {|l| l == lengths[0]}
+      :equilateral
+    elsif lengths[1] == lengths[2] || lengths[0] == lengths[2] || lengths[0] == lengths[1]
+      :isosceles
+    elsif lengths.all? {|l| lengths.reject {|r| l == r}.all?{|v| v != l}}
+      :scalene
+    end
   end
 
 end
